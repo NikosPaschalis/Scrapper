a468a0d8fd618d020b0c9efb00e7e38a31eb5a83
Νικόλαος Πασχάλης <n.pashalis@DYPE.LOCAL>
2020-08-27 12:05:19 +0300

    Finalize amazon titles and pagination scrapping

--- index.js
+++ index.js
@@ -4,27 +4,42 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     //{headless: false} in order to open the window
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.bookdepository.com/bestsellers');
-  var BDlastPageNumber = 34;
-  //pausing window in order to not close fast
-    //await browser.waitForTarget(() =>false);
-    const result = await page.evaluate(() =>{
-        let heading = document.querySelectorAll('.item-info');
-        const headingList = [...heading];
-        return headingList.map(h =>"Title: " + h.childNodes[1].innerText + " Author: "+ h.childNodes[3].innerText + " Price: " + h.childNodes[11].children[0].innerText);
+  
+  // const page = await browser.newPage();
+  // await page.goto('https://www.bookdepository.com/bestsellers');
+  // //pausing window in order to not close fast
+  //   //await browser.waitForTarget(() =>false);
+  //   const result = await page.evaluate(() =>{
+  //       let heading = document.querySelectorAll('.item-info');
+  //       const headingList = [...heading];
+  //       return headingList.map(h =>"Title: " + h.childNodes[1].innerText + " Author: "+ h.childNodes[3].innerText + " Price: " + h.childNodes[11].children[0].innerText);
         
-    });
-    console.log(result);
+  //   });
+  //   console.log(result);
 
-    // const amazon = await browser.newPage();
-    // await amazon.goto('https://www.amazon.com/gp/bestsellers/2020/books');
-    //   const Amazonresult = await amazon.evaluate(() =>{
-    //       let heading = document.querySelectorAll('.p13n-sc-truncated');
-    //       const headingList = [...heading];
-    //       return headingList.map(h => h.innerText);
-    //   });
-    //   console.log(Amazonresult);
+    const extractAmazonTitles = async (url) => {
+      const amazon = await browser.newPage();
+      await amazon.goto(url);
+      const AmazonTitles = await amazon.evaluate(() =>
+           Array.from(document.querySelectorAll('.zg-item-immersion .p13n-sc-truncated'))
+           .map(b => b.innerText.trim(''))         
+      );
+      await amazon.close();
+      if (AmazonTitles.length < 1) {
+        // Terminate if no partners exist
+        return AmazonTitles
+      } else {
+        // Go fetch the next page ?page=X+1
+        const nextPageNumber = parseInt(url.match(/pg=(\d+)$/)[1], 10) + 1;
+        const nextUrl = `https://www.amazon.com/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_pg_2?_encoding=UTF8&pg=${nextPageNumber}`;
+  
+        return AmazonTitles.concat(await extractAmazonTitles(nextUrl))
+      }
+      return AmazonTitles;
+    }
+    const browser = await puppeteer.launch();
+    const url = 'https://www.amazon.com/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_pg_2?_encoding=UTF8&pg=1'; 
+    const Amazonresult = await extractAmazonTitles(url);
+    console.log(Amazonresult);
   await browser.close();
 })();
